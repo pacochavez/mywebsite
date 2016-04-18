ed16f98a7dc69bcaa81eb3c0f955a721f4cbff97
pacochavez <fco.chavezg@hotmail.com>
2016-04-17 20:14:54 -0700

    timeline for mobile

--- app/public/js/animateScroll.js
+++ app/public/js/animateScroll.js
@@ -1,14 +1,11 @@
 var AnimateElements = function(){
 	var view = window.outerHeight;
 	var ElementsAnimate = document.getElementsByClassName('animateElement');
-			// console.log(view)
-	
 	for(var i = 0 ;i < ElementsAnimate.length;i++){
 		var E = ElementsAnimate[i].getBoundingClientRect();
 		var S = ElementsAnimate[i].className.split(" ");
 		if(S[0]!="active"){
 			if(E.top < (view-100) && E.top != 0){
-				console.log(E.top)
 					ElementsAnimate[i].className ='active '+ElementsAnimate[i].className;
 			}
 		}
